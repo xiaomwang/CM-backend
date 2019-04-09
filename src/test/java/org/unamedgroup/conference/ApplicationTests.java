b02b84612dbb102af1cc126d65be25c755f4cb4d
1059438020 <1059438020@qq.com>
2019-04-09 21:54:48 +0800

    错误修改

--- src/test/java/org/unamedgroup/conference/ApplicationTests.java
+++ src/test/java/org/unamedgroup/conference/ApplicationTests.java
@@ -17,8 +17,6 @@ import java.io.FileNotFoundException;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-    @Autowired
-    ConferenceService conferenceService;
 
     @Autowired
     ConferenceRepository conferenceRepository;
@@ -28,8 +26,7 @@ public class ApplicationTests {
 
     @Test
     public void contextLoads() throws ParseException {
-        System.out.println(conferenceService.getConferenceList("2019-03-13 00:00:00","2019-03-13 23:30:00"));
-        System.out.println(conferenceService.splitConferenceList("2019-03-13 00:00:00"));
+
     }
     @Test
     public void faceDetecture() throws FileNotFoundException {
