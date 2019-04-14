faf1aae9aec5b51b1e30c10fff01b4a3cc82e9da
Andy Liu <andyliu24@outlook.com>
2019-04-14 18:15:46 +0800

    完善查询空闲会议室结果

--- src/main/java/org/unamedgroup/conference/controller/RoomController.java
+++ src/main/java/org/unamedgroup/conference/controller/RoomController.java
@@ -9,6 +9,7 @@ import org.unamedgroup.conference.entity.temp.SuccessInfo;
 import org.unamedgroup.conference.service.GuideQueryService;
 import org.unamedgroup.conference.service.QuickCheckService;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -33,9 +34,34 @@ public class RoomController {
     @ResponseBody
     public Object getFreeRoom(@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date start, @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date end) {
         if (guideQueryService.getFreeRoomIDByDate(start, end) == null) {
-            return new FailureInfo(6000, "查询空闲房间失败！请核实！");
+            return new FailureInfo(6000, "没有房间空闲或查询失败！请核实！");
         } else {
             return new SuccessInfo(guideQueryService.getFreeRoomIDByDate(start, end));
+        }
+    }
+
+    @RequestMapping(value = "/freeRoomNumber", method = RequestMethod.GET)
+    @ResponseBody
+    public Object getFreeRoomNumberToday() {
+        Date current = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(current);
+        // 日期后移一天
+        calendar.add(Calendar.DAY_OF_MONTH, +1);
+        // 小时置零
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        // 分置零
+        calendar.set(Calendar.MINUTE, 0);
+        // 秒置零
+        calendar.set(Calendar.SECOND, 0);
+        // 毫秒置零
+        calendar.set(Calendar.MILLISECOND, 0);
+
+
+        if (guideQueryService.getFreeRoomIDByDate(current, calendar.getTime()) == null) {
+            return new FailureInfo(6000, "没有房间空闲或查询失败！请核实！");
+        } else {
+            return new SuccessInfo(guideQueryService.getFreeRoomIDByDate(current, calendar.getTime()).size());
         }
     }
 
